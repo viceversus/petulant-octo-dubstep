2bcb7cd823f8ba8889d3252e1c1e719d9c72f09e
Rob Pak <rob@carbonfive.com>
2014-10-21 12:50:13 -0700

    Add new player controller

---
+++ petulant-octo-dubstep/controllers/NewPlayerController.swift
@@ -0,0 +1,26 @@
+//
+//  NewPlayerController.swift
+//  petulant-octo-dubstep
+//
+//  Created by Rob Pak on 10/21/14.
+//  Copyright (c) 2014 viceversus. All rights reserved.
+//
+
+import UIKit
+
+class NewPlayerController: UIViewController {
+    
+    @IBOutlet var nameTextField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
