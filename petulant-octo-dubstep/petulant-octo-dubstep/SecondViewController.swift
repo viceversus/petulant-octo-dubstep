30001e29894684ffedda75d29ab4ded1a94a62bb
Ken Shimizu <kenshimizu88@gmail.com>
2014-10-21 11:06:58 -0700

    Create project with template

---
+++ petulant-octo-dubstep/petulant-octo-dubstep/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  petulant-octo-dubstep
+//
+//  Created by Ken Shimizu on 10/21/14.
+//  Copyright (c) 2014 viceversus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
