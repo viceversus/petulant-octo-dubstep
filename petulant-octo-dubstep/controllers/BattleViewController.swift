7ff3c2bacfbe20b68e0977bcc9be71ee87ac5625
Chad Jablonski <chad@carbonfive.com>
2014-10-21 13:41:54 -0700

    Battlefield buttons

---
+++ petulant-octo-dubstep/controllers/BattleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BattleViewController.swift
+//  petulant-octo-dubstep
+//
+//  Created by Chad Jablonski on 10/21/14.
+//  Copyright (c) 2014 viceversus. All rights reserved.
+//
+
+import UIKit
+
+class BattleViewController: UIViewController {
+    @IBOutlet weak var attackButton: UIBarButtonItem!
+    @IBOutlet weak var runButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var battlefield = Battlefield()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func attack(sender: AnyObject) {
+        println("ATTACK!")
+    }
+}
